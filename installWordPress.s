46ce19bbb3f58f914fe0cbdb62207eadd10d1c9b
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2023-11-29 22:22:54 -0300

    Create installWordPress.s

---
+++ installWordPress.s
@@ -0,0 +1,6 @@
+#https://ubuntu.com/server/docs/how-to-install-and-configure-wordpress
+#https://ubuntu.com/server/docs/introduction-to-web-services
+#https://ubuntu.com/server/docs/databases-mysql
+sudo apt install apache2
+sudo apt install mysql-server
+sudo apt install wordpress
